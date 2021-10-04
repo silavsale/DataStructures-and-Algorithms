0e2f7a420ff89d97c1b31ae2a269246a6800a714
Tiger <silavsale@gmail.com>
2021-10-03 22:32:47 -0400

    added hash tables

--- HashTables/HashTable.js
+++ HashTables/HashTable.js
@@ -13,27 +13,71 @@ HashTable.prototype.hash = function (key) {
   let total = 0
   for (let i = 0; i < key.length; i++) {
     total += key.charCodeAt(i)
-    console.log(total)
+    // console.log(total)
   }
-  console.log(total)
+  // console.log(total)
   const bucket = total % this.numBuckets
+  // console.log(bucket)
   return bucket
 }
 
 HashTable.prototype.insert = function (key, value) {
   const index = this.hash(key)
+  console.log("INDEX:", index)
   if (!this.buckets[index]) this.buckets[index] = new HashNode(key, value)
-  else {
-    const currentNode = this.buckets[index]
-    while (current.next) {
-      currentNode = current.next
+  else if (this.buckets[index].key === key) {
+    this.buckets[index].value = value
+  } else {
+    let currentNode = this.buckets[index]
+    while (currentNode.next) {
+      if (currentNode.next.key === key) {
+        currentNode.next.value = value
+        return
+      }
+      currentNode = currentNode.next
     }
     currentNode.next = new HashNode(key, value)
   }
 }
 
+HashTable.prototype.get = function (key) {
+  const index = this.hash(key)
+  if (!this.buckets[index]) return null
+  else {
+    let currentNode = this.buckets[index]
+    while (currentNode) {
+      if (currentNode.key === key) {
+        return currentNode.value
+      }
+      currentNode = currentNode.next
+    }
+    return null
+  }
+}
+
+HashTable.prototype.retrieveAll = function () {
+  const nodes = []
+  for (let i = 0; i < this.buckets.length; i++) {
+    let currentNode = this.buckets[i]
+    while (currentNode) {
+      nodes.push(currentNode)
+      currentNode = currentNode.next
+    }
+  }
+  return nodes
+}
+
 const myHt = new HashTable(30)
 
-console.log(myHt.hash("Becca"))
-console.log(462 % 30)
-console.log(myHt)
+myHt.insert("Dean", "dean@gmail.com")
+myHt.insert("Megan", "megan@gmail.com")
+myHt.insert("Dane", "dane@gmail.com")
+// myHt.insert("Dean", "dean2@gmail.com")
+myHt.insert("Enda", "endaChamp@gmail.com")
+myHt.insert("Joe", "joey@gmail.com")
+myHt.insert("Samantha", "sammy@gmail.com")
+
+// console.log(myHt.get("Enda"))
+
+console.log(myHt.retrieveAll())
+// console.log(myHt.buckets)
---
+++ Remainder.js
@@ -0,0 +1,16 @@
+// Remainder (%)
+// The remainder operator (%) returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend.
+
+console.log(10 % 3) // = 1 ; because 3 * 3 gets you 9, and 10 - 9 is 1.
+
+console.log(12 % 5)
+// expected output: 2
+
+console.log(-12 % 5)
+// expected output: -2
+
+console.log(4 % 2)
+// expected output: 0
+
+console.log(-4 % 2)
+// expected output: -0
