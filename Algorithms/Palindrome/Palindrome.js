eed9caf9e5e515d50776e0b2c2258b44ed9a0974
Tiger <silavsale@gmail.com>
2021-10-22 09:54:15 -0400

    add palindrome alg

---
+++ Algorithms/Palindrome/Palindrome.js
@@ -0,0 +1,14 @@
+function isPalandrome(string) {
+  string = string.toLowerCase()
+  const charArr = string.split("")
+  const validChar = "abcdefghijklmnopqrstuvwxyz".split("")
+  const letterArr = []
+  charArr.forEach((char) => {
+    if (validChar.indexOf(char) > -1) letterArr.push(char)
+  })
+
+  if (letterArr.join("") === letterArr.reverse().join("")) return true
+  else return false
+}
+
+isPalandrome("iMadam i'm Adami")
