ed72321fd9938ecdc05d75f236acf5577b923407
Tiger <silavsale@gmail.com>
2021-11-01 00:08:06 -0400

    add CeasarCipher algorithm

---
+++ Algorithms/CeasarCipher/CeasarCipher.js
@@ -0,0 +1,30 @@
+function ceasarCipher(str, num) {
+  num = num % 26
+
+  let lowerCaseString = str.toLowerCase()
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+
+  let newString = ""
+
+  for (var i = 0; i < lowerCaseString.length; i++) {
+    let currentLetter = lowerCaseString[i]
+    if (currentLetter === " ") {
+      newString += currentLetter
+      continue
+    }
+
+    let currentIndex = alphabet.indexOf(currentLetter)
+    let newIndex = currentIndex + num
+    if (newIndex > 25) newIndex = newIndex - 26
+    if (newIndex < 0) newIndex = 26 + newIndex
+
+    if (str[i] === str[i].toUpperCase()) {
+      newString += alphabet[newIndex].toUpperCase()
+    } else newString += alphabet[newIndex]
+  }
+
+  return newString
+}
+
+ceasarCipher("Zoo Keeper", 2)
+ceasarCipher("Javascript", -900)
