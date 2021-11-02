005bd1aff957066e19265a47b170b25d94304f8b
Tiger <silavsale@gmail.com>
2021-11-01 23:03:30 -0400

    add ReverseWords algorithm

---
+++ Algorithms/ReverseWords/ReverseWords.js
@@ -0,0 +1,25 @@
+const reverseWords = (string) => {
+  const arrayOfWords = string.split(" ")
+  let reversedArrayOfWords = ""
+
+  for (let i = 0; i < arrayOfWords.length; i++) {
+    if (arrayOfWords[i] !== undefined) {
+      arrayOfWords[i].length
+      let reversedWord = ""
+      for (let y = arrayOfWords[i].length; y >= 0; y--) {
+        if (arrayOfWords[i][y] !== undefined) {
+          reversedWord += arrayOfWords[i][y]
+        }
+      }
+      if (i !== arrayOfWords.length - 1) {
+        reversedWord += " "
+      }
+      reversedArrayOfWords += reversedWord
+    }
+  }
+
+  console.log(reversedArrayOfWords)
+  return reversedArrayOfWords
+}
+
+reverseWords("this is a string of words")
---
+++ Algorithms/ReverseWords/ReverseWords_2.js
@@ -0,0 +1,16 @@
+const reverseWords = (string) => {
+  const arrayOfWords = string.split(" ")
+  let reversedArrayOfWords = []
+
+  arrayOfWords.forEach((word) => {
+    let reversedWord = ""
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i]
+    }
+    reversedArrayOfWords.push(reversedWord)
+  })
+
+  return reversedArrayOfWords.join(" ")
+}
+
+reverseWords("this is a string of words")
