71dfe249c9d0b995138387deb8df1e653cc58622
MoonWalker <silavsale@gmail.com>
2023-03-14 13:52:19 -0400

    add tests for twosum

--- __test__/two-sum.spec.ts
+++ __test__/two-sum.spec.ts
@@ -2,13 +2,33 @@ import { describe, expect, test } from "@jest/globals"
 import { twoSum } from "../master-the-coding-interview-big-tech-faang-interviews/two-sum"
 
 describe("TwoSum function", () => {
+  test("The test uses Array [1, 3, 7, 9, 2] and 11 as the target number, and expects the function to return [3, 4].", () => {
+    const returnValue = twoSum([1, 3, 7, 9, 2], 11)
+    expect(returnValue).toEqual([3, 4])
+  })
+
   test("TwoSum should return Array type", () => {
-    const returnValue = twoSum([1, 2, 3], 10)
+    const returnValue = twoSum([1, 2, 3], 3)
     expect(returnValue).toBeInstanceOf(Array)
   })
 
   test("If no posible sum exists return null", () => {
     const returnValue = twoSum([1, 2, 3], 100000)
-    expect(returnValue).toBeInstanceOf(null)
+    expect(returnValue).toBeNull()
+  })
+
+  test("If Array is empty return null", () => {
+    const returnValue = twoSum([], 100000)
+    expect(returnValue).toBeNull()
+  })
+
+  test("If Array has only 1 element return null", () => {
+    const returnValue = twoSum([5], 5)
+    expect(returnValue).toBeNull()
+  })
+
+  test("If target value is 1 of less return null", () => {
+    const returnValue = twoSum([1, 2], 1)
+    expect(returnValue).toBeNull()
   })
 })
--- master-the-coding-interview-big-tech-faang-interviews/two-sum.ts
+++ master-the-coding-interview-big-tech-faang-interviews/two-sum.ts
@@ -2,10 +2,31 @@
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-import { number } from "yargs"
+const nums = [2, 7, 11, 15],
+  target = 9
 
-export const twoSum = (arr: number[], target: number): number[] => {
-  let indexes = []
-
-  return indexes
+interface MyObject {
+  [key: number]: number
 }
+
+export const twoSum = (arr: number[], target: number): number[] | null => {
+  let map: MyObject = {}
+
+  if (target < 2 || arr.length < 2) {
+    return null
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] === undefined) {
+      map[arr[i]] = i
+    }
+
+    if (map[target - arr[i]] !== undefined) {
+      return [map[target - arr[i]], i]
+    }
+  }
+
+  return null
+}
+
+twoSum(nums, target)
