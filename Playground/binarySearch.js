77a92ad093fa48ee893f923e4ddb7092fd9cda42
Silavsale <silavsale@gmail.com>
2023-06-15 22:55:08 -0400

    add playground folder

---
+++ Playground/binarySearch.js
@@ -0,0 +1,12 @@
+/* eslint-disable semi */
+const arr = [1, 3, 5, 7, 11, 16, 22];
+
+const search = (array, target) => {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+  }
+
+  return 1;
+};
+
+search(arr, 5);
