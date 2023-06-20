a92377bd62f1334841d7ebe28f0a6a0fb181752a
MoonWalker <silavsale@gmail.com>
2023-06-20 00:38:21 -0400

    1732 completed

--- 1732-find-the-highest-altitude.ts
+++ 1732-find-the-highest-altitude.ts
@@ -1,5 +1,4 @@
 // const gain = [-5, 1, 5, 0, -7];
-const gain = [-4, -3, -2, -1, 4, 3, 2];
 // Output: 1
 // Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 // Example 2:
@@ -8,13 +7,22 @@ const gain = [-4, -3, -2, -1, 4, 3, 2];
 // Output: 0
 // Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
 
+// const gain = [-4, -3, -2, -1, 4, 3, 2];
+const gain = [-5, 1, 5, 0, -7];
+
 function largestAltitude(gain: number[]): number {
-  let heightDifference = 0;
-  for (let i = 1; i < gain.length; i++) {
-    
+  let highestAltitude = 0;
+  let height = 0;
+  for (let i = 0; i < gain.length; i++) {
+    height = height + gain[i];
+    if (height > highestAltitude) {
+      highestAltitude = height;
+    }
   }
 
-  return heightDifference;
+  return highestAltitude;
 }
 
 largestAltitude(gain);
+
+0 + -5 + 1 + 5 + 0 + -7;
