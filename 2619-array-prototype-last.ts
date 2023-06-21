a9808ab529921f78e8cc856920bebc8af71663bb
Silavsale <silavsale@gmail.com>
2023-06-20 22:22:04 -0400

    add 2619 array-prototype-last

---
+++ 2619-array-prototype-last.ts
@@ -0,0 +1,38 @@
+// array-prototype-last
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element.
+//  If there are no elements in the array, it should return -1.
+
+// You may assume the array is the output of JSON.parse.
+
+// Input: nums = [null, {}, 3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+// Example 2:
+
+// Input: nums = []
+// Output: -1
+// Explanation: Because there are no elements, return -1.
+
+declare global {
+  interface Array<T> {
+    last(): T | -1;
+  }
+}
+
+Array.prototype.last = function () {
+  if (this.length === 0) {
+    return -1;
+  } else {
+    return this[this.length - 1];
+  }
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+export {};
+let arr = [1, 2, 3];
+[1, 2, 3, {}, 4, 5].last();
+[null, {}, 3].last();
